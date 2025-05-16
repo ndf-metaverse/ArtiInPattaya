72fe2d06164f28245047deb120df96351503b0da
Carubee <jeff555a@gmail.com>
2025-05-16 12:18:20 +0700

    add speed animal type

--- Assets/MovingObject.cs
+++ Assets/MovingObject.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
     private bool fromLeft;
-    private float speed;
+    public float speed;
     private Camera cam;
+
+    private float screenBuffer = 100f;
+    private int bounceCount = 0;
+    private const int maxBounce = 3;
+    private bool hasReturned = false;
 
     public void Initialize(bool fromLeft, float speed, Camera cam)
     {
@@ -17,19 +22,41 @@ public class MovingObject : MonoBehaviour
 
     void Update()
     {
-        float moveDir = fromLeft ? 1f : -1f;
-        if(fromLeft)
-        {
-            transform.Translate(Vector3.right * moveDir * speed * Time.deltaTime);
-        }
-        else
-        {
-            transform.Translate(Vector3.left * moveDir * speed * Time.deltaTime);
-        }
+        float moveAmount = speed * Time.deltaTime;
+        
+        transform.Translate(Vector3.right * moveAmount);
 
         Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
-        
-            Destroy(gameObject,60);
-        
+
+        if (!hasReturned && (screenPos.x < -screenBuffer || screenPos.x > Screen.width + screenBuffer))
+        {
+            hasReturned = true;
+
+            fromLeft = !fromLeft;
+
+            Vector3 pos = transform.position;
+            pos.z += fromLeft ? 1f : -1f;
+            transform.position = pos;
+
+            Vector3 scale = transform.localScale;
+            scale.z = -scale.z;
+            transform.localScale = scale;
+
+            if (pos.z <= 0)
+            {
+                transform.Rotate(0f, 180f, 0f);
+            }
+            bounceCount++;
+            if (bounceCount >= maxBounce)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (hasReturned && screenPos.x >= 0 && screenPos.x <= Screen.width)
+        {
+            hasReturned = false;
+        }
+
     }
 }
